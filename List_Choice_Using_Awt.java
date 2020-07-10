f2be47e3fa22ea807b62aec140ed33f2bc656127
Deepanshuchauhan123 <deepanshuchauhan7287@gmail.com>
2020-07-10 19:18:07 +0530

    Mouse State and Dragged position

--- List_Choice_Using_Awt.java
+++ List_Choice_Using_Awt.java
@@ -1,16 +1,16 @@
 import java.awt.*;
 import java.awt.event.*;
 
-class List_Choice_Using_Awt implements ItemListener{
+   class List_Choice_Using_Awt implements ItemListener {
 
     Frame frame;
     List list;
     Choice choice;
 
-    List_Choice_Using_Awt (){
-        frame=new Frame("Sample Program");
-        list=new List();
-        choice=new Choice();
+    List_Choice_Using_Awt() {
+        frame = new Frame("Sample Program");
+        list = new List();
+        choice = new Choice();
 
         list.add("Jaipur");
         list.add("Jaisalmer");
@@ -26,10 +26,14 @@ class List_Choice_Using_Awt implements ItemListener{
         choice.add("Alwar");
         choice.add("Udaipur");
 
+        Font font = new Font(Font.SERIF, Font.ITALIC, 24);
+        choice.setFont(font);
+        list.setFont(font);
+        
         frame.setLayout(null);
 
-        list.setBounds(20,150,80,100);
-        choice.setBounds(120,150,100,50);
+        list.setBounds(20, 150, 200, 300);
+        choice.setBounds(300, 150, 200, 100);
 
         frame.add(list);
         frame.add(choice);
@@ -37,25 +41,23 @@ class List_Choice_Using_Awt implements ItemListener{
         list.addItemListener(this);
         choice.addItemListener(this);
 
-        frame.setSize(300,300);
+        frame.setSize(800, 800);
         frame.setVisible(true);
     }
 
 
-
     public static void main(String[] args) {
-    List_Choice_Using_Awt myClass=new List_Choice_Using_Awt ();
+        List_Choice_Using_Awt myClass = new List_Choice_Using_Awt();
     }
 
     @Override
     public void itemStateChanged(ItemEvent e) {
-        if(e.getSource()== list){
+        if (e.getSource() == list) {
             choice.add(list.getSelectedItem());
             list.remove(list.getSelectedItem());
-        }else{
+        } else {
             list.add(choice.getSelectedItem());
             choice.remove(choice.getSelectedItem());
         }
     }
 }
-
